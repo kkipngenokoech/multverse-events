2b61e1e2ad364a9121e0feb0d0ad7a00496b2521
kip <kkipngenokoech22@gmail.com>
2023-08-14 21:42:00 +0300

    time to sleep I need to wake up in the morning for flutter

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e3dfd484c1527639"; }
+/******/ 		__webpack_require__.h = function() { return "84196970f7eb2374"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Index() {
   const profile = data[0];
   return (
     <div className="container speaker-list">
-      <S peakerCard profile={profile} />
+      <SpeakerCard profile={profile} />
     </div>
   );
 }
