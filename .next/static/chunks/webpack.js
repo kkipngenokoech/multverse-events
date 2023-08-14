4f387b57d6a0ff3ee555b1210fbf77827c8febcb
kip <kkipngenokoech22@gmail.com>
2023-08-14 18:43:41 +0300

    latest updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "43516519bca147e3"; }
+/******/ 		__webpack_require__.h = function() { return "731ee829490beed1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
